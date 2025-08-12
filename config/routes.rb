59412e85b607f322297bb36c973cc437479d0fe8
madeforyalo <madeforyalo@gmail.com>
2025-08-12 12:51:08 -0300

    cambios

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
-
 Rails.application.routes.draw do
-  resources :favorites, only: [:index, :create, :destroy] do
-    collection do
-      post :toggle
-    end
+  resources :favorites, only: [:index] do
+    collection { post :toggle }
   end
 end
--- lib/redmine_favorites_kaizen2b.rb
+++ lib/redmine_favorites_kaizen2b.rb
@@ -2,9 +2,15 @@
 
 require 'redmine'
 
-# Constante que coincide con el nombre del archivo lib/redmine_favorites_kaizen2b.rb
+# Constante para zeitwerk
 module RedmineFavoritesKaizen2b
 end
 
-# Cargamos el hook (ruta → namespace RedmineFavorites::Hooks)
+# Hacer disponible el helper en cualquier vista (incluida la del proyecto)
+Rails.configuration.to_prepare do
+  require_dependency File.expand_path('../app/helpers/favorites_helper', __dir__)
+  ActionView::Base.include ::FavoritesHelper
+end
+
+# Hooks
 require_relative 'redmine_favorites/hooks/view_projects_show_right_hook'
