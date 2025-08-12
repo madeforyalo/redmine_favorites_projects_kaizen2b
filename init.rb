46eb46322990dcde63151093a8cc7288d39a3d34
madeforyalo <madeforyalo@gmail.com>
2025-08-12 12:04:44 -0300

    cambio de nombre

--- init.rb
+++ init.rb
@@ -2,21 +2,18 @@
 
 require_relative 'lib/redmine_favorites_kaizen2b'
 
-Redmine::Plugin.register :redmine_favorite_projects_kaizen2b do
-  name        'Favorites projects'
-  author      'Kaizen2B - Gonzalo Rojas'
+Redmine::Plugin.register :redmine_favorites_projects_kaizen2b do
+  name        'Favorites Projects (Kaizen2B)'
+  author      'Kaizen2B'
   description 'Permite a cada usuario marcar proyectos como favoritos y accederlos rápido.'
   version     '0.1.0'
   url         'https://kaizen2b.com'
   author_url  'https://kaizen2b.com'
+  requires_redmine version_or_higher: '6.0.0'
 
-  # Permiso global (no por proyecto): requiere usuario logueado
-  requires_redmine version_or_higher: '5.0.0'
-
-  # Menú superior
+  # Menú superior opcional
   menu :top_menu, :favorite_projects,
        { controller: 'favorites', action: 'index' },
        caption: :label_favorite_projects,
        if: proc { User.current.logged? }
-
 end
