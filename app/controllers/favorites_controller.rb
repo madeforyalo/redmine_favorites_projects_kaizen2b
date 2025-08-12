48d1240e766d8ded00b2e7224530c43e5ffa371a
madeforyalo <madeforyalo@gmail.com>
2025-08-12 15:07:49 -0300

    Revert "feat(favorites): ⭐ en listado de Proyectos con toggle AJAX"
    This reverts commit 2c5f077e7aaf0435ce2e2c7c9055967815788d5b.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,62 +2,35 @@
 
 class FavoritesController < ApplicationController
   before_action :require_login
-  respond_to :html, :json
 
   def index
     @projects = Project.active.joins("INNER JOIN favorite_projects fp ON fp.project_id = projects.id")
                        .where('fp.user_id = ?', User.current.id)
                        .order('projects.name')
-
-    respond_to do |format|
-      format.html
-      format.json do
-        render json: {
-          project_ids: @projects.pluck(:id),
-          project_identifiers: @projects.pluck(:identifier)
-        }
-      end
-    end
   end
 
-  # POST /favorites/toggle(.json)
-  # Params: project_id OR project_identifier
+  # POST /favorites/toggle?project_id=ID
   def toggle
-    project = find_project_from_params!
+    project = Project.find(params[:project_id])
 
     fav = FavoriteProject.find_by(user_id: User.current.id, project_id: project.id)
-    favourited = false
-
     if fav
       fav.destroy
       flash[:notice] = l(:notice_project_unfavourited)
     else
       FavoriteProject.create!(user_id: User.current.id, project_id: project.id)
-      favourited = true
       flash[:notice] = l(:notice_project_favourited)
     end
 
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: project_path(project)) }
-      format.json do
-        render json: {
-          favourited: favourited,
-          project_id: project.id,
-          project_identifier: project.identifier
-        }
-      end
-    end
+    redirect_back(fallback_location: project_path(project))
   end
 
-  private
+  # Compat: no usamos create/destroy directos, pero quedan por si se requieren
+  def create
+    toggle
+  end
 
-  def find_project_from_params!
-    if params[:project_id].present?
-      Project.find(params[:project_id])
-    elsif params[:project_identifier].present?
-      Project.find_by!(identifier: params[:project_identifier])
-    else
-      raise ActiveRecord::RecordNotFound, 'project param missing'
-    end
+  def destroy
+    toggle
   end
 end
